df88a6ab913a470360b1507230a4fc5b00cb534b
yogarajan <yogaraja.g@gmail.com>
2026-06-03 13:21:54 +0530

    changes2

--- cursorproject/Controllers/WeatherForecastController.cs
+++ cursorproject/Controllers/WeatherForecastController.cs
@@ -29,5 +29,16 @@ namespace cursorproject.Controllers
             })
             .ToArray();
         }
+        [HttpGet("All", Name = "GetAllWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetAllWeatherForecast()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
